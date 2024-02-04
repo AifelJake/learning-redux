c76bee3ed1e3213ee534d6c9f2bcb9737e21ba5d
AifelJake <aifeljake7@gmail.com>
2024-02-04 17:43:28 +0800

    added more slices

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement} from "./counterSlice"
-
+import { increment, decrement, reset, incrementByAmount} from "./counterSlice"
+import { useState } from 'react'
 
 const Counter = () => {
 
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+    const [incrementAmount, setIncrementAmount] = useState(0);
+
+    const addValue = Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+      setIncrementAmount(0);
+      dispatch(reset());
+    }
 
   return (
     <section>
@@ -14,6 +22,19 @@ const Counter = () => {
         <div>
             <button onClick={() => dispatch(increment())}>+</button>
             <button onClick={() => dispatch(decrement())}>-</button>
+            <input
+              type='text'
+              value={incrementAmount}
+              onChange={(e) => setIncrementAmount(e.target.value)}
+            />
+            
+            <div>
+            <button onClick={() => dispatch(incrementByAmount(addValue))}>incrementByAmount</button>
+            <button onClick={() => dispatch(resetAll)}>reset</button>
+            </div>
+
+            {/* <button onClick={() => dispatch(reset())}>reset</button>
+            <button onClick={() => dispatch(incrementByAmount())}>incrementByAmount</button> */}
         </div>
         
     </section>
